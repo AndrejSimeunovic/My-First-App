4a44c5347bd559143542b4052fb8c65194f9a4ed
Andrej-Simeunovic <125864768+Andrej-Simeunovic@users.noreply.github.com>
2023-04-02 11:52:52 +0200

    Added button to display accelerometer values when clicked

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -2,13 +2,58 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startButton = findViewById(R.id.start_button);
+        startButton.setOnClickListener(view -> startAccelerometer());
+
+        Button stopButton = findViewById(R.id.stop_button);
+        stopButton.setOnClickListener(view -> stopAccelerometer());
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    private void startAccelerometer() {
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    private void stopAccelerometer() {
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            ((TextView) findViewById(R.id.txt)).setText("X: " + x + ", Y: " + y + ", Z: " + z);
+
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        // Do nothing
     }
 }
